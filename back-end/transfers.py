b200345d028727a4e186bbbc8dd1fb6764daf4cb
juless89 <j.m.r.kramer@gmail.com>
2019-03-03 01:35:06 +0100

    Error fix

--- back-end/transfers.py
+++ back-end/transfers.py
@@ -43,7 +43,7 @@ class Transfers(threading.Thread):
 
             if not self.scraping:
                 self.counter.dump_data()
-                self.db.dump('api_tranfers')
+                self.db.dump('api_transfers')
             else:
                 # For each minute of data processed upload the data into the
                 # database and clear the buffers.
